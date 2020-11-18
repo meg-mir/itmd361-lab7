5bdd75bb4d37f8a5b0b89ed3cee8c4c67baf61db
Meg <70302885+meg-mir@users.noreply.github.com>
2020-11-18 17:54:50 -0600

    added java

--- js/script.js
+++ js/script.js
@@ -1,11 +1,23 @@
 function init(){
 //add your javascrip between these two lines of code
- 
-
-
-
-
-
-
-
+ .center {
+  text-align: center;
+}
+#container {
+  width: 400px;
+  margin: 50px auto 0;
+}
+#entryinput {
+  width: 100%;
+}
+#entrybutton {
+  width: 200px;
+  text-align: center;
+  color: #fff;
+  background-color: #c00;
+  padding: 2em 0;
+  margin: 20px auto;
+  font-size: 24px;
+  font-weight: bold;
+}
 window.addEventListener('load', init);
