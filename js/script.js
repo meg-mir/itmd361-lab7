1684d9fb7573ab60d7cec8a55ce36958b2b4de85
Meg <70302885+meg-mir@users.noreply.github.com>
2020-11-18 20:36:28 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 document.getElementById("entrybutton").addEventListener("click", alert);
+
 function myFunction() {
   alert("Megan Miranda: You successfully submitted your RSVP!");
 }
+
 function myFunction() {
   document.getElementById("textoutput");
+}
