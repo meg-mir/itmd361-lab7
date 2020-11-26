cf31e0578a8525f3fc04e023f9f6bb4d929b59c6
Meg <70302885+meg-mir@users.noreply.github.com>
2020-11-25 19:52:55 -0600

    added .value of var entry to grab text
    instead of action.

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ function init(){
 var button = document.getElementById('entrybutton');
 var txoutput = document.getElementById('textoutput');
   button.addEventListener('click', function(){
-    var entry = document.getElementById('entryinput');
+    var entry = document.getElementById('entryinput').value;
     alert(' Megan Miranda: ' + entry);
     txoutput.innerHTML = entry;
   });
