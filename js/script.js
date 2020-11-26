cce8df31bbba35798cbf2bb9667fe678ac952c53
Meg <70302885+meg-mir@users.noreply.github.com>
2020-11-25 21:07:11 -0600

    removed various variables/condensed
    javascript

--- js/script.js
+++ js/script.js
@@ -1,13 +1,8 @@
-function init() {
-//add your javascrip between these two lines of code
-  document.getElementById("entrybutton").addEventListener("click", f1);
-  function f1() {
-    var hObj =  document.querySelectorAll("h2.center");
-    var button = document.getElementById('entrybutton');
-    var txoutput = document.getElementById('textoutput');
-    var entry = document.getElementById('entryinput').value;
-      alert(' Megan Miranda: ' + entry);
-      txoutput.innerHTML = entry;
-  }
-window.addEventListener('load', init);
-}
+function init(){
+  var x = document.getElementById("entrybutton"); 
+  x.addEventListener('click', function(){
+    alert("Megan Miranda: "+ document.getElementById("entryinput").value)
+    document.getElementById("textoutput").innerHTML = document.getElementById("entryinput").value;
+  });
+} 
+  window.addEventListener('load', init);
