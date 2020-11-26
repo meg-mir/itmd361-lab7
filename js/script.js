212f77d6a086fbc12b6fbebbf5b5d589284bc2cf
Meg <70302885+meg-mir@users.noreply.github.com>
2020-11-25 19:51:37 -0600

    fixed order of var entry

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 function init(){
-var entry = document.getElementById('entryinput');
 var button = document.getElementById('entrybutton');
 var txoutput = document.getElementById('textoutput');
-button.addEventListener('click', function(){
-alert(' Megan Miranda: ' + entry);
-txoutput.innerHTML = entry;
-});
+  button.addEventListener('click', function(){
+    var entry = document.getElementById('entryinput');
+    alert(' Megan Miranda: ' + entry);
+    txoutput.innerHTML = entry;
+  });
 }
 window.addEventListener('load', init);
