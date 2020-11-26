b0c409b96d12901fa9c1554dd3c1d73279592fe9
Meg <70302885+meg-mir@users.noreply.github.com>
2020-11-25 20:57:13 -0600

    fixed indentation/edit to "entry"/added
    query selector

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
-function init(){
-var button = document.getElementById('entrybutton');
-var txoutput = document.getElementById('textoutput');
-  button.addEventListener('click', function(){
+function init() {
+//add your javascrip between these two lines of code
+  document.getElementById("entrybutton").addEventListener("click", f1);
+  function f1() {
+    var hObj =  document.querySelectorAll("h2.center");
+    var button = document.getElementById('entrybutton');
+    var txoutput = document.getElementById('textoutput');
     var entry = document.getElementById('entryinput').value;
-    alert(' Megan Miranda: ' + entry);
-    txoutput.innerHTML = entry;
-  });
-}
+      alert(' Megan Miranda: ' + entry);
+      txoutput.innerHTML = entry;
+  }
 window.addEventListener('load', init);
+}
